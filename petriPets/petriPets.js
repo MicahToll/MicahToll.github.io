1a5e8170fa6ba6fb0e51ca35b894843889196e0d
MicahToll <i.egxist@gmail.com>
2021-11-02 17:31:39 -0700

    added a fractal and began tomogatchy.

---
+++ petriPets/petriPets.js
@@ -0,0 +1,23 @@
+/*
+this will be a list of inputs and outputs for the neural network:
+inputs:
+1,2,3,4,5,6,7,8
+
+
+outs:
+fight/eat - high cost
+reproduce - very high cost
+move - cheap
+rest - reduce living cost
+increase turn order - free
+decrease turn order - free
+
+other:
+turn order
+energy reserves
+
+
+in      mid     mid     out
+in      mid     mid     out
+in      mid     mid     out
+*/
