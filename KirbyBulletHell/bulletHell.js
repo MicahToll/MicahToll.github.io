785ecfe34367e9b511ee4c95172b6d7a32a540d6
MicahToll <i.egxist@gmail.com>
2022-05-21 10:33:29 -0700

    added KirbybulletHell

---
+++ KirbyBulletHell/bulletHell.js
@@ -0,0 +1,193 @@
+//define ambiant sound
+//var music = document.getElementById("myAudio");
+
+//setting up three.js
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000);			
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });//to add anti aliasing, sdd { antialias: true } to the parameter
+renderer.setSize(window.innerWidth, window.innerHeight); 
+document.body.appendChild( renderer.domElement );//document.getElementById("body").appendChild( renderer.domElement );
+
+//everything in the universe's reference frame should be added to this group
+var universe = new THREE.Group();//the size should be approximately equal to the size of the solar system i think (which is 15,000 lsecond radius), on second thought, lets aim for closer to 1,500
+
+//create spaceship
+var spaceship = new THREE.Group();
+
+//add atuff to the scene
+scene.add( universe );
+
+
+var all_bullets = [];
+var all_enemies = [];
+let player_x = 0;
+let player_y = 0;
+//let player_dx = 0;
+//let player_dy = 0;
+let half_window_width = 0;
+let half_window_height = 0;
+let player_radius = 5;
+let health = 100;
+let ship;
+let time = 0;
+function init(){
+    camera.position.z = 5;
+    camera.lookAt(0,0,0);
+    half_window_width = window.innerWidth/2;
+    half_window_height = window.innerHeight/2;
+    /*universe.position.x = half_window_width;//this makes zero zero at the middle of the screen
+    universe.position.y = half_window_height;*/
+    //document.body.addEventListener("mousemove", updatePlayer, false);
+
+    gameloop(1);
+
+    const material = new THREE.SpriteMaterial( {color: 0xff0000} );
+    const sprite = new THREE.Sprite(material);
+    //sprite.scale.set(200, 200, 1)
+    universe.add( sprite );
+
+    const vertices = [];
+    for ( let i = 0; i < 10000; i ++ ) {
+        const x = THREE.MathUtils.randFloatSpread( 2000 );
+        const y = THREE.MathUtils.randFloatSpread( 2000 );
+        const z = THREE.MathUtils.randFloatSpread( 2000 );
+
+        vertices.push( x, y, z );
+    }
+    var starGeometry = new THREE.BufferGeometry();
+	starGeometry.setAttribute('position', new THREE.Float32BufferAttribute( vertices, 3 ));
+	var starMaterial = new THREE.PointsMaterial({color:0x888888})
+	var stars = new THREE.Points( starGeometry, starMaterial );
+	universe.add(stars);
+}
+
+function gameloop(timestamp){
+    for (var enemy of all_enemies){
+        //enemy.update();
+    }
+    for (var bullet of all_bullets){
+        //bullet.update_bullet();
+        /*if (bullet.check_collision()){
+            //console.log("boom");
+            health -= 1;
+            update_dash();
+        }*/
+    }
+    for (var i = all_bullets.length; i--;) {
+        /*if (all_bullets[i].check_onscreen()){
+            all_bullets[i].destroy_graphics();
+            all_bullets.splice(i, 1);
+        }*/
+    }
+    time = timestamp;
+    renderer.render(scene,camera);
+    window.requestAnimationFrame(gameloop);
+}
+
+function updatePlayer(event) {
+    player_x = event.x-half_window_width;
+    player_y = event.y-half_window_height;
+    ship.x = player_x;
+    ship.y = player_y;
+}
+
+function update_dash(){
+    document.getElementById("health").innerHTML = "health: "+health+"%"
+}
+
+var c1 = 0xe32d2d;
+var c2 = 0x32a852;
+var c3 = 0x10a9e0;
+var c4 = 0x710dbd;
+
+//currently assuming all 
+class Bullet{
+    constructor(radius, vectors, geometry){
+        this.radius = radius;
+        this.vectors = vectors;
+        this.dimensions = vectors.length;
+        this.graphics = new PIXI.Graphics(geometry);
+        everything.addChild(this.graphics);
+    }
+    update_bullet(){
+        let v = this.vectors;
+        let d = this.dimensions;
+        if (d>1){
+            for (var i = d-1;i>0;i--){
+                v[i-1][0] += v[i][0];
+                v[i-1][1] += v[i][1];
+            }
+            this.graphics.x = v[0][0];
+            this.graphics.y = v[0][1];
+        }
+    }
+    check_collision(){
+        if ((this.graphics.x-player_x)**2+(this.graphics.y-player_y)**2 < (this.radius+player_radius)**2){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    check_onscreen(){
+        if (Math.abs(this.graphics.x)+this.radius>half_window_width||Math.abs(this.graphics.y)+this.radius>half_window_height){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    destroy_graphics(){
+        this.graphics.destroy();
+    }
+}
+
+class Enemy{//radius, vectors, geometry
+    constructor(enemy_geometry, hp, enemy_radius, period, movement, rotation, bullet_geometry){
+        this.hp = hp;
+        this.enemy_radius = enemy_radius;
+        this.period = period;
+        this.movement = movement;
+        this.rotation = rotation;
+        this.bullet_geometry = bullet_geometry;
+        this.graphics = new PIXI.Graphics(enemy_geometry);
+        everything.addChild(this.graphics);
+    }
+    update(){
+        this.move();
+        this.shoot();
+    }
+    move(){
+        this.graphics.x = half_window_width*Math.cos(time*2*Math.PI/this.period);
+        this.graphics.y = 0;
+        //this.graphics.y = half_window_height;
+        /*for (a of movement.x.a){
+            //something
+        }
+        something = movement.x.a[0]/2 + Math.cos() + Math.sin()
+        //do something
+        */
+    }
+    rotate(){}
+    shoot(){
+        if (Math.cos(time*2*Math.PI/(this.period/8))**2<.05){
+            all_bullets.push(new Bullet(10, [[this.graphics.x,-half_window_height/2],[.3,.2],[0,.01]], this.bullet_geometry));
+        }
+    }
+    
+    /*check_collision(){
+        if ((this.graphics.x-player_x)**2+(this.graphics.y-player_y)**2 < (this.radius+player_radius)**2){
+            return true;
+        } else {
+            return false;
+        }
+    }*/
+    destroy_graphics(){
+        this.graphics.destroy();
+    }
+}
+/*class sub_enemy extends enemy{
+    constructor(){
+        super()
+    }
+}*/
