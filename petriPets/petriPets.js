3b63e4ee1bc2fe3eaeb6a7842da8d0285ba82238
MicahToll <i.egxist@gmail.com>
2023-10-11 11:41:43 -0700

    added background from prior project

--- petriPets/petriPets.js
+++ petriPets/petriPets.js
@@ -5,13 +5,6 @@
 class Key {
     constructor() {
         this.value = false;
-        /*this.w = false;
-        this.a = false;
-        this.s = false;
-        this.d = false;
-        this.q = false;
-        this.e = false;
-        this.space = false;*/
     }
 }
 let w_key = new Key();
@@ -123,6 +116,7 @@ const kirby_bullet_lightblue_material = new THREE.SpriteMaterial( { map: kirby_b
 //bond material
 const bond_material_1 = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
+let background_things = [];
 let creatures = [];
 let cells = [];
 let bonds = [];
@@ -147,13 +141,7 @@ function init() {
     window_height = window.innerHeight;
     camera_width = camera_height*window_width/window_height
 
-    /*const kirby_map = new THREE.TextureLoader().load( 'assets/kirby.png' );
-    const kirby_material = new THREE.SpriteMaterial( { map: kirby_map } );
-    kirby = new THREE.Sprite( kirby_material );
-    kirby.scale.set(player_radius*2, player_radius*2, 1);
-    kirby.position.y = -camera_height/4
-    universe.add( kirby );*/
-
+    set_up_level1()
     set_up_level2();
     gameloop(0);
 }
@@ -162,9 +150,9 @@ let repel_dist_vector = new THREE.Vector3(0, 0, 0);
 
 function gameloop(timestamp) {
     //update background
-    /*for (thing of background_things){
+    for (thing of background_things){
         thing.update();
-    }*/
+    }
 
     for (let i = bonds.length-1; i > 0-1; i--) {
         let bond = bonds[i];
@@ -222,7 +210,6 @@ class Schematic {//we are going to change this
                     current_cell.update_schematics(this, x, y);
                     current_cell.add_cell_to_simulation();
                     current_cell.position_vector.addScaledVector(x_basis, x).addScaledVector(y_basis, -y).add(creature_position);
-                    console.log(current_cell.velocity_vector);
                     current_cell.velocity_vector.add(creature_velocity)
                 }
             }
@@ -451,7 +438,7 @@ class Directed_Cell extends Cell {
         this.update_direction();
     }
     clone_cell() {
-        return new Cell(this.mass, this.k, this.bond_length, this.max_length, this.charge, this.sprite_material, this.sprite_diameter, this.position_vector, this.velocity_vector, this.anchor_bond_index, this.desired_angle);
+        return new Directed_Cell(this.mass, this.k, this.bond_length, this.max_length, this.charge, this.sprite_material, this.sprite_diameter, this.position_vector, this.velocity_vector, this.anchor_bond_index, this.desired_angle);
     }
 }
 
@@ -462,6 +449,9 @@ class Player_Cell extends Cell { //there should only ever be one player vector (
     update_cell() {
         camera.position.setX(this.position_vector.x);
         camera.position.setY(this.position_vector.y);
+    }
+    clone_cell() {//this function should probably only be called once tops. (unless I add split screen)
+        return new Player_Cell(this.mass, this.k, this.bond_length, this.max_length, this.charge, this.sprite_material, this.sprite_diameter, this.position_vector, this.velocity_vector);
     }
 }
 
@@ -478,6 +468,9 @@ class Key_Cell extends Cell {
             this.output = 0;
         }
     }
+    clone_cell(){
+        return new Key_Cell(this.mass, this.k, this.bond_length, this.max_length, this.charge, this.sprite_material, this.sprite_diameter, this.position_vector, this.velocity_vector, this.key);
+    }
 }
 
 class Propulsor extends Directed_Cell {
@@ -490,6 +483,9 @@ class Propulsor extends Directed_Cell {
             super.update_cell();
             this.force_vector.addScaledVector(this.direction, this.propulsion);
         }
+    }
+    clone_cell() {
+        return new Propulsor(this.mass, this.k, this.bond_length, this.max_length, this.charge, this.sprite_material, this.sprite_diameter, this.position_vector, this.velocity_vector, this.anchor_bond_index, this.desired_angle, this.propulsion);
     }
 }
 
@@ -602,129 +598,6 @@ function set_up_level2() {/*
     }
 }*/
 
-//currently assuming all 
-class Bullet{
-    constructor(radius, vectors, material, damage){
-        this.radius = radius;
-        this.vectors = vectors;
-        this.dimensions = vectors.length;
-        this.damage = damage;
-        this.sprite = new THREE.Sprite(material);
-        this.sprite.scale.set(radius*2, radius*2, 1);
-        universe.add(this.sprite);
-    }
-    update_bullet(){
-        let v = this.vectors;
-        let d = this.dimensions;
-        if (d>1){
-            for (var i = d-1;i>0;i--){
-                v[i-1][0] += v[i][0];
-                v[i-1][1] += v[i][1];
-            }
-            this.sprite.position.x = v[0][0];
-            this.sprite.position.y = v[0][1];
-        }
-    }
-    check_collision(){
-        if ((this.sprite.position.x-player_x)**2+(this.sprite.position.y-player_y)**2 < (this.radius+player_radius)**2){
-            health -= this.damage;
-            update_dash();
-            return true;
-        } else {
-            return false;
-        }
-    }
-    check_onscreen(){
-        if (Math.abs(this.sprite.position.x)+this.radius>camera_width/2||Math.abs(this.sprite.position.y)+this.radius>camera_height/2){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    delete_sprite(){
-        universe.remove(this.sprite);
-    }
-}
-class Kirby_bullet extends Bullet{
-    check_collision(){
-        //return false
-        let x_pos = this.sprite.position.x;
-        let y_pos = this.sprite.position.y;
-        for (var i = all_enemies.length; i--;) {
-            let enemy = all_enemies[i];
-            if ((x_pos-enemy.x)**2+(y_pos-enemy.y)**2 < (this.radius+enemy.radius)**2){
-                enemy.hp -= this.damage;
-                if (enemy.check_health()){
-                    enemy.delete_sprite();
-                    all_enemies.splice(i, 1);
-                }
-                return true;
-            }
-        }
-        return false;
-    }
-}
-
-class Enemy{//radius, vectors, geometry
-    constructor(material, hp, radius, update, bullet_materials, damage = 1, init_x = 0, init_y = 0){
-        this.hp = hp;
-        this.radius = radius;
-        //this.period = period;
-        //this.movement = movement;
-        //this.rotation = rotation;
-        this.bullet_materials = bullet_materials;
-        this.sprite = new THREE.Sprite(material);
-        this.sprite.scale.set(radius*2, radius*2, 1);
-        this.damage = damage;
-        universe.add(this.sprite);
-        this.update = update;
-        this.state = 0;
-        this.frame_count = 0;
-        this.x = init_x;
-        this.y = init_y;
-        this.move();
-    }
-    update(){}
-    move(){
-        this.sprite.position.x=this.x;
-        this.sprite.position.y=this.y;
-    }
-    /*move(){
-        this.graphics.x = half_window_width*Math.cos(time*2*Math.PI/this.period);
-        this.graphics.y = 0;
-        //this.graphics.y = half_window_height;
-        for (a of movement.x.a){
-            //something
-        }
-        something = movement.x.a[0]/2 + Math.cos() + Math.sin()
-        //do something  
-    }*/
-    /*rotate(){}*/
-    /*shoot(){
-        if (Math.cos(time*2*Math.PI/(this.period/8))**2<.05){
-            all_bullets.push(new Bullet(10, [[this.graphics.x,-half_window_height/2],[.3,.2],[0,.01]], this.bullet_geometry));
-        }
-    }*/
-    check_collision(){
-        if ((this.sprite.position.x-player_x)**2+(this.sprite.position.y-player_y)**2 < (this.radius+player_radius)**2){
-            health -= this.damage;
-            update_dash();
-            return true;
-        } else {
-            return false;
-        }
-    }
-    check_health(){
-        if (this.hp <= 0){
-            return true;
-        }
-    }
-    delete_sprite(){
-        universe.remove(this.sprite);
-    }
-}
-
 class Background {
     constructor(update,mesh){
         this.update = update;
@@ -755,7 +628,7 @@ function set_up_level1() {
 	var starMaterial = new THREE.PointsMaterial({color:0x888888})
 	var stars = new THREE.Points( starGeometry, starMaterial );
 	background.add(stars);
-
+    /*
     let planel1_geometry = new THREE.IcosahedronGeometry(75);
     let planel1_edges = new THREE.EdgesGeometry( planel1_geometry );
     let planel1_material = new THREE.LineBasicMaterial( { color: 0x00ff00 } );
@@ -765,43 +638,9 @@ function set_up_level1() {
         this.mesh.rotation.y += .001;
     }, planet1_line));
 
-    let b_bullet_map = new THREE.TextureLoader().load( 'assets/bullets/bbullet.png' );
-    let b_bullet_material = new THREE.SpriteMaterial( { map: b_bullet_map } );
-    let dark_matter_map = new THREE.TextureLoader().load( 'assets/enemies/darkmatter.png' );
-    let dark_matter_material = new THREE.SpriteMaterial( { map: dark_matter_map } );
-    let dark_matter_tear_map = new THREE.TextureLoader().load( 'assets/bullets/darkmattertear.png' );
-    let dark_matter_tear_material = new THREE.SpriteMaterial( { map: dark_matter_tear_map } );
-
-    all_enemies.push(new Enemy(
-        dark_matter_material, 1000, 8, function(){
-            if(this.state == 0) {//phase 1
-                this.x = camera_width/2*3/4*Math.sin(Math.PI/60/4*this.frame_count)
-                if(this.frame_count%4==0){
-                    all_bullets.push(new Bullet(1, [[this.x,this.y],[0,-.5]], this.bullet_materials[0],10));
-                }
-                this.move();
-                this.frame_count++
-                if(this.frame_count >= 10*60){
-                    this.state = 1;
-                }
-            }
-            else if(this.state == 1){//phase 2
-                this.x = camera_width/2*3/4*Math.sin(Math.PI/60/4*this.frame_count)
-                this.y = 25+camera_width/6*3/4*Math.sin(Math.PI/60/3*this.frame_count)
-                if(this.frame_count%4==0){
-                    all_bullets.push(new Bullet(1, [[this.x,this.y],[0,-.5]], this.bullet_materials[1],15));
-                }
-                this.move();
-                this.frame_count++
-            }
-        }, [
-            b_bullet_material,
-            dark_matter_tear_material
-        ], 1, 0, 25));
-
-    background.position.z = -200;//offset
+    background.position.z = -200;//offset*/
     //move_background = function() { background.position.y -= .1 }//translate
-    move_background = function() { background.rotation.x += .001 }//rotate
+    //move_background = function() { background.rotation.x += .001 }//rotate
 
     //music.play();
 }
